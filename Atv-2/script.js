d9e5462c024ebed0a295ad1d3426e255982ceec4
Sanderson de Oliveira Machado <sanderson.oliveira@dinfor.uniceub>
2024-08-27 08:29:12 -0300

    update

--- Atv-2/script.js
+++ Atv-2/script.js
@@ -1,40 +1,66 @@
-function submeter() {
+function submeter(){
+    
     let nome = document.getElementById("nome").value;
-    let titulo = document.getElementById("titulo").value;
+    let idade = document.getElementById("idade").value;
     let cpf = document.getElementById("cpf").value;
+    
+    console.log(cpf);
+    console.log(validacpf(cpf))
 
-    //console.log(titulo)
-    //console.log(nome)
-    console.log(validaCPF(cpf))
 }
 
-function validaCPF(cpf) {
+function validacpf(cpf){
 
-/* Continuar validação
-formatos CPFs validos:
-123.456.89-10
-12345678910
-*/
-    if (cpf == "") {
-        alert("Campo CPF não pode ser vazio")
-        return false
+    /*
+    continuar validação:
+    CPF validos:
+        123.456.789-10
+        12345678910
+    
+    Fazer:
+
+    - validar se a posição do . e - estão corretas
+
+    - validar se o cpf está correto
+        */
+    if (cpf == "" ){
+        alert("Campo CPF não pode ser vazio");
+        return false;
     }
-
+    
     cpf = cpf.trim();
 
-    if (/[a-zA-Z]/.test(cpf)) {
-        alert("CPF não pode ter letras");
+    if (/[a-zA-Z]/.test(cpf)){
+        alert("CPF não pode conter letras");
+        return false;
     }
     
-
-    if (!/^[\d.-]+$/.test(cpf)) {
-        alert("Caracter invalido")
-    } 
-
-    if(cpf.length != 1 && !cpf.length != 14) {
-        alert("Formato invalido")
+   array = Array.from(cpf)
+   
+    if (cpf.length != 14 || !/^[\d.-]+$/.test(cpf) ){
+        if ((cpf[3] || cpf[7] != ".") && cpf[11] != "-") {
+            alert("Formato errado, apenas aceito ***.***.***-**")
+            return false
+        }
+        alert("Tamanho errado")
+        return false
+    } else if (cpf.length != 14 || cpf.length != 11) {
+        alert("Tamanho incorreto, apenas aceito tamanho de 11 e 14 nos respectivos formatos")
         return false
     }
     
-    return true
+    cpf = cpf.replace("/[.-]/g","");
+    console.log(cpf);
+
+    let soma = 0;
+
+    for (i = 1; i <= 9; i++) {
+        console.log(cpf.charAt(i-1));
+        soma = soma + (cpf.charAt(i-1) * (10 - (i-1)))
+    }
+
+    console.log(soma);
+    let resto = soma % 11;
+
+    return true; 
 }
